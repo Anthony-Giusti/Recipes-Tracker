623c96e34cec56c066ee018a6a57a8cc6c1efb7a
AlbatrossMissle <69980441+AlbatrossMissle@users.noreply.github.com>
2021-05-31 16:24:59 -0500

    Import update to reflect name change of styles file and deleted unnecessary lint comments

--- src/components/RecipeTags/DietTags.jsx
+++ src/components/RecipeTags/DietTags.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-import useStyles from './Styles-Tags';
+import useStyles from './Styles';
 
 const DietTags = ({ dietTags }) => {
   const classes = useStyles();
