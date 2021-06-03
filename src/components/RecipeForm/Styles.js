a569d63fd6025535ee6c4aad0c3cddd80cef968c
AlbatrossMissle <69980441+AlbatrossMissle@users.noreply.github.com>
2021-06-03 15:43:31 -0500

    Style changes and cleaup of unused styles

--- src/components/RecipeForm/Styles.js
+++ src/components/RecipeForm/Styles.js
@@ -1,17 +1,11 @@
 import { makeStyles } from '@material-ui/core';
-import { grey, red } from '@material-ui/core/colors';
-import Theme from '../../Themes/Theme';
+import grey from '@material-ui/core/colors/grey';
 
 export default makeStyles((theme) => ({
-  title: {
-    textDecoration: 'underline',
-    marginBottom: '20',
-  },
   section: {
     margin: '1em 0',
     padding: '1em',
   },
-  sectionTitle: { fontSize: '2.25em' },
   field: {
     marginTop: 20,
     marginBottom: 20,
@@ -20,6 +14,12 @@ export default makeStyles((theme) => ({
   yieldCookTimeContainer: {
     display: 'flex',
     flexWrap: 'wrap',
+  },
+  secondaryTitle: {
+    background: theme.palette.primary.main,
+    color: theme.palette.getContrastText(theme.palette.primary.main),
+    padding: '5px 20px',
+    borderRadius: '1em',
   },
   yieldCookTime: {
     display: 'flex',
@@ -34,13 +34,12 @@ export default makeStyles((theme) => ({
     borderRadius: '0',
     '&:hover': {
       color: 'white',
-      background: Theme.palette.error.main,
+      background: theme.palette.error.main,
     },
   },
   ingredientSearchField: {
     maxWidth: 200,
   },
-
   searchResultsItem: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -56,7 +55,6 @@ export default makeStyles((theme) => ({
     flexDirection: 'row',
   },
   checkBoxGroup: {
-    // background: '#ede8ea',
     background: grey[200],
     padding: 10,
     margin: 5,
@@ -68,32 +66,7 @@ export default makeStyles((theme) => ({
   checkBoxOptions: {
     flexDirection: 'row',
   },
-  URLField: {
-    // display: 'flex',
-  },
   addImageURLBtn: {
     borderRadius: '0',
   },
-  ingredients: {
-    marginBottom: '1em',
-  },
-  addStepBtn: {
-    borderRadius: '10%',
-    color: 'white',
-    background: Theme.palette.primary.main,
-    '&:hover': {
-      background: Theme.palette.secondary.main,
-    },
-  },
-  newStepContainer: {
-    width: '100%',
-  },
-  newStep: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  newStepField: {
-    flexGrow: '1',
-  },
-  submitBtn: {},
 }));
